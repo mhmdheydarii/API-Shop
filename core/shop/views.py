b07d0b637d7526eb15504acb3e159a5f1f9f26e2
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-08-01 12:02:11 -0700

    fixe

--- core/shop/views.py
+++ core/shop/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from django.core.exceptions import FieldError
 
 from .serializers import ProductListSerializer, ProductDetailSerializer
 from .models import ProductModel
