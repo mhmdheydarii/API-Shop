654720732c7b247b2ff2871309c0f9268cdcae8b
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-08-02 15:14:31 -0700

    created model for accounts app

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Profile 
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "email", "is_staff", "is_active", "is_verified" ,"type", "created_date"]
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "last_name", "created_date"]
+    search_fields = ["first_name"]
+
+    
