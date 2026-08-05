dadd38de42bb76bc340f15384ffe1ca418f81f5c
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-08-05 06:31:21 -0700

    create : create cart app models

--- core/cart/admin.py
+++ core/cart/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CartModel, CartItemModel
 
 # Register your models here.
+
+@admin.register(CartModel)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["user", "created_date", "updated_date"]
+
+@admin.register(CartItemModel)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ["cart", "product", "quantity", "created_date", "updated_date"]
--- core/cart/models.py
+++ core/cart/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from accounts.models import User
+from shop.models import ProductModel
 # Create your models here.
+
+class CartModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_cart")
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+    def calculate_total_price(self):
+        return sum(item.product.get_price() * item.quantity for item in self.cart_items.all())
+
+    class Meta:
+        ordering = ["-created_date"]
+
+
+class CartItemModel(models.Model):
+    cart = models.ForeignKey(CartModel, on_delete=models.CASCADE, related_name="cart_items")
+    product = models.ForeignKey(ProductModel, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=1)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name
+
+    class Meta:
+        ordering = ["-created_date"]
+    
