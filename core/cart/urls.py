84f110839e5cc7e7bfa1f9eb166b3e93fce6cf18
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-08-07 08:29:34 -0700

    update : Update views and URLs for better structure

--- core/cart/urls.py
+++ core/cart/urls.py
@@ -5,8 +5,6 @@ from . import views
 app_name = "cart"
 
 urlpatterns = [
-    path("add-item/", views.AddProductView.as_view(), name="add-item"),
-    path("update-item/", views.UpdateProductView.as_view(), name="update-item"),
-    path("remove-item/", views.RemoveProductView.as_view(), name="remove-item"),
-    path("items/", views.ProductListView.as_view(), name="cart-items"),
+    path("items/", views.CartItemsView.as_view(), name="cart-items"),
+    path("item/", views.CartView.as_view(), name="cart-item"),
 ]
--- core/cart/views.py
+++ core/cart/views.py
@@ -6,60 +6,11 @@ from rest_framework.response import Response
 from .serializers import CartProductSerializer
 from .cart import CartSession
 from shop.models import ProductModel
+
 # Create your views here.
 
-class AddProductView(APIView):
 
-    def post(self, request):
-        cart = CartSession(request.session)
-        serializer = CartProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        product = serializer.validated_data["product"]
-
-        product_stock = product.stock
-
-        result = cart.add_product(product.id, product_stock)
-
-        if not result:
-            return Response({"message": "Cannot add product to cart. Product may be out of stock or unavailable."}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({"message":"Product added successfully"})
-
-
-class UpdateProductView(APIView):
-
-    def patch(self, request):
-        cart = CartSession(request.session)
-        serializer = CartProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        product = serializer.validated_data["product"]
-
-        result = cart.update_product_quantity(product.id)
-
-        if not result:
-            return Response({"message": "Product stock limit reached"}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({"message":"Product quantity updated"}, status=status.HTTP_200_OK)
-
-
-class RemoveProductView(APIView):
-
-    def delete(self, request):
-        cart = CartSession(request.session)
-        serializer = CartProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        product = serializer.validated_data["product"]
-
-        result = cart.remove_product(product.id)
-
-        if not result:
-            return Response({"message":"Product not found"}, status=status.HTTP_404_NOT_FOUND)
-            
-        return Response({"message":"Product removed successfully"}, status=status.HTTP_200_OK)
-
-
-class ProductListView(APIView):
+class CartItemsView(APIView):
 
     def get(self, request):
         cart = CartSession(request.session)
@@ -78,3 +29,52 @@ class ProductListView(APIView):
             }, 
             status=status.HTTP_200_OK
             )
+
+
+class CartView(APIView):
+
+    # Add product in cart
+    def post(self, request):
+        cart = CartSession(request.session)
+        serializer = CartProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        product = serializer.validated_data["product"]
+
+        product_stock = product.stock
+
+        result = cart.add_product(product.id, product_stock)
+
+        if not result:
+            return Response({"message": "Cannot add product to cart. Product may be out of stock or unavailable."}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"message":"Product added successfully"})
+    
+    # Update product quantity in cart
+    def patch(self, request):
+        cart = CartSession(request.session)
+        serializer = CartProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        product = serializer.validated_data["product"]
+
+        result = cart.update_product_quantity(product.id)
+
+        if not result:
+            return Response({"message": "Product stock limit reached"}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"message":"Product quantity updated"}, status=status.HTTP_200_OK)
+    
+    # Delete product from cart
+    def delete(self, request):
+        cart = CartSession(request.session)
+        serializer = CartProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        product = serializer.validated_data["product"]
+
+        result = cart.remove_product(product.id)
+
+        if not result:
+            return Response({"message":"Product not found"}, status=status.HTTP_404_NOT_FOUND)
+            
+        return Response({"message":"Product removed successfully"}, status=status.HTTP_200_OK)
+    
