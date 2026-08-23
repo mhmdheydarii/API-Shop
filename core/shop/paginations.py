8d0c943e91bb1b16d845bcd8b94fd533b5a55ace
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-08-23 13:17:17 -0700

    Update : Shop app pagination

--- core/shop/paginations.py
+++ core/shop/paginations.py
@@ -2,5 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class ProductsPagination(PageNumberPagination):
     page_query_param = "page"
-    page_size = 1
+    page_size = 24
     max_page_size = 100
